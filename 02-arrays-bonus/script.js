8367b5662ca4c0f1b02bb625a179e12a5f207902
AlessandroMessineo <alessandromessineo46@gmail.com>
2025-08-29 19:03:58 +0200

    es 4 e 5 di arrays bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -35,7 +35,22 @@ console.log(teachers);
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+let present;
+for(i = 0; i < teachers.length; i++){
+  if(teachers[i] === 'Fabio'){
+    present = "Fabio è presente";
+  }
+}
+const isFabioPresent = present;
+console.log(isFabioPresent);
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+let saveTeachersString ="";
+for (let i = 0; i < teachers.length; i++) {
+  saveTeachersString += teachers[i];
+  if (i < teachers.length - 1) {
+    saveTeachersString += ", ";
+  }
+}
+const teachersString = saveTeachersString;
+console.log(teachersString);
