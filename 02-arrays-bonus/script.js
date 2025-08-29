c5490402d328e8ffb0b28a54547511d56ed099bf
AlessandroMessineo <alessandromessineo46@gmail.com>
2025-08-29 17:46:42 +0200

    primi 3 es di arrays bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,13 +10,28 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = null;
+let saveReverseTeachers = [];
+for(i = teachers.length-1; i > -1; i--){
+  saveReverseTeachers.push(teachers[i]);
+}
+const reversedTeachers = saveReverseTeachers;
+console.log(reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+let saveLongNames = [];
+for(i = 0; i < teachers.length; i++){
+  if(teachers[i].length >= 5){
+    saveLongNames.push(teachers[i]);
+  }
+}
+const longNames = saveLongNames;
+console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
+const deletEd = teachers.indexOf("Ed");
+teachers.splice(deletEd, 1)
+console.log(teachers);
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
